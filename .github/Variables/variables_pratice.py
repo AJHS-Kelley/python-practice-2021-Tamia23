0f7f07ffccb5025cba5bb64259cee5a5fa49fe45
Tamia.o <90413525+Tamia23@users.noreply.github.com>
2021-10-28 14:48:11 -0400

    Create variables_pratice.py

---
+++ .github/Variables/variables_pratice.py
@@ -0,0 +1,11 @@
+# Variables Practice, Tamia Ogletree, 1/28/21 2:21pm, version 0.0
+
+# Common Data Types and Variables in Python
+
+life_count = 12 # Integer value, all whole numbers, +/- and 0.
+account_balance = 2679.87 # Floating point value, +/-.
+user_name = "Marry amy jane" # String, a collection of letters/numbers.
+
+print(life_count)
+print(account_balance)
+print(user_name)
